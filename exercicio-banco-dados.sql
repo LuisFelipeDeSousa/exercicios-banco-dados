e335804b4d483340e7b3191732c9b41c4cdfcc99
LuisFelipeDeSousa <luisfelipecontatosystem@hotmail.com>
2018-06-21 16:39:36 -0300

    fd

--- exercicio-banco-dados.sql
+++ exercicio-banco-dados.sql
@@ -1,4 +1,13 @@
-﻿CREATE TABLE Pessoas(Nome VARCHAR(100), 
+﻿/*Nome da coluna Tipo da coluna
+nome					varchar(100)
+cpf						varchar(14)
+rg						varchar(12)
+data de nascimento		DateTime
+idade					tinyint*/
+
+
+
+CREATE TABLE Pessoas(Nome VARCHAR(100), 
 					Cpf VARCHAR(14), 
 					Rg VARCHAR(12),
 					DataNascimento DATETIME,
