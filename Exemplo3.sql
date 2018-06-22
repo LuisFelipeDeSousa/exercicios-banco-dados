e561f21f509d912a0af740677d980d39b6383e6a
LuisFelipeDeSousa <luisfelipecontatosystem@hotmail.com>
2018-06-22 17:25:13 -0300

    gfgd

---
+++ Exemplo3.sql
@@ -0,0 +1,43 @@
+﻿/*DROP TABLE vendas;
+
+CREATE TABLE vendas(
+	nome		VARCHAR (100),
+	preco		DECIMAL (6,2),
+	quantidade	SMALLINT
+	);
+
+	INSERT INTO vendas VALUES 
+	('Café', 4, 27.13),
+	('Leite', 35, 4.15),
+	('Açucar', 52, 24.67),
+	('Biscoito', 3, 1.71),
+	('Miojo', 9, 45.23),
+	('Calabresa', 17, 1.72),
+	('Bacon', 18, 19.52),
+	('Feijão', 18, 7.87),
+	('Sazon', 47, 4.16),
+	('Qboa', 50, 22.39);
+
+	SELECT * FROM vendas;
+
+	SELECT
+	nome			'Nome',
+	quantidade		'Quantidade',
+	preco			'Preço',
+	quantidade * preco 'Total'
+
+	FROM vendas
+	ORDER BY 'Total' DESC;
+
+	SELECT SUM (preco) 'Somatória dos preços' FROM vendas; -- Somar todos os registros por uma coluna
+
+	SELECT SUM(preco * quantidade) FROM vendas;*/ -- Somar preços
+
+	SELECT AVG (Preco) FROM vendas; -- Média dos preços por coluna
+
+
+
+
+
+
+
---
+++ Exemplo4.sql
@@ -0,0 +1,58 @@
+﻿DROP TABLE produtos;
+
+CREATE TABLE produtos(
+		nome VARCHAR(100)
+
+);
+
+INSERT INTO produtos VALUES
+('Calabresa'),
+('X-Calabresa'),
+('Pizza de calabresa'),
+('Calabresa com Miojo'),
+('Milk shake de calabresa'),
+('Paçoca de calabresa com açai'),
+('Polenta com paçoca');
+
+SELECT * FROM produtos;
+
+SELECT * FROM produtos WHERE nome LIKE 'Calabresa%'; -- Busca os produtos que contém o nome inciando com CALA...
+
+-- Buscar os produtos que contém o nome finalizando com CALABRESA
+
+SELECT * FROM produtos WHERE nome LIKE '%Calabresa';
+
+-- Buscar os produtos que contém Calabresa em qualquer parte
+
+SELECT * FROM produtos WHERE nome LIKE '%Calabresa%';
+
+
+-------------------------------- RETORNO DE PREÇO ---- > Continuação do Exemplo Vendas>
+
+SELECT MIN(preco)FROM vendas; -- Retorna o menor preço
+SELECT MAX(preco)FROM vendas; -- Retorna o maior preço
+
+
+SELECT 
+nome, LEN(nome) -- LEN retorna quantidade de caracteres da coluna
+FROM vendas 
+WHERE LEN(nome) > 5
+ORDER BY LEN(nome) DESC;
+
+SELECT nome
+FROM vendas
+WHERE LEN(nome) = (SELECT MAX(LEN(nome)) FROM vendas); -- Retorna o menor nome
+
+SELECT nome 
+FROM vendas
+WHERE LEN(nome) = (SELECT MIN(LEN(nome)) FROM vendas) -- Retorna o maior nome
+
+AND nome LIKE 'C%'; -- Retorna nomes que começam com C.
+
+/*% que começa no inicio -- Buscar atrás
+% que no final -- Buscar na frente*/
+
+
+                                                         
+
+
