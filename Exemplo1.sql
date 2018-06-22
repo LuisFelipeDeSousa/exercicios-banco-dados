f2ec826edf9d135f5f2bae29287d9dbe449ca97a
LuisFelipeDeSousa <luisfelipecontatosystem@hotmail.com>
2018-06-22 16:48:58 -0300

    fgdgf

---
+++ Exemplo1.sql
@@ -0,0 +1,21 @@
+﻿DROP TABLE jogos;
+
+CREATE TABLE quedas(
+		nome VARCHAR (100) DEFAULT 'Neymar', /*----> DEFAULT é o nome que vai apresentar na queda NULL, quando não*/
+		quedas SMALLINT                           /*passo o nome ele considera o nome que colquei no DEFAULT*/
+		);
+
+		-- Atomaticamente o nome recebe Neymar
+		-- Pois não é passado o nome no insert
+
+		INSERT INTO jogos()
+		(quedas)
+		VALUES (20);
+
+
+		INSERT INTO jogos
+		(quedas, nome)
+		VALUES(2, 'Tite');
+
+		SELECT * FROM jogos;
+
